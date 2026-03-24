1c6f232bef1028b2da205cd40198a0d3d91043de
SakshamNeupane-01 <neupanasakcham@gmail.com>
2026-03-24 16:07:32 +0530

    Use Case 2: Basic Room Types & Static Availability

--- APP/src/BookApp.java
+++ APP/src/BookApp.java
@@ -1,4 +1,90 @@
-package PACKAGE_NAME;
+/**
+ * Book My Stay Application
+ * Use Case 2: Basic Room Types & Static Availability
+ *
+ * Demonstrates abstraction, inheritance, and polymorphism.
+ *
+ * @author YourName
+ * @version 1.1
+ */
 
+// Abstract Class
+abstract class Room {
+    protected String roomType;
+    protected int beds;
+    protected double price;
+
+    // Constructor
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    // Method to display room details
+    public void displayDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: ₹" + price);
+    }
+}
+
+// Single Room Class
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+}
+
+// Double Room Class
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+}
+
+// Suite Room Class
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+}
+
+// Main Application Class
 public class BookApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== Welcome to Book My Stay =====\n");
+
+        // Creating Room Objects (Polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Static Availability (simple variables)
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        // Display Room Details
+        System.out.println("---- Room Details ----\n");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println("----------------------");
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println("----------------------");
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
+        System.out.println("----------------------");
+
+        System.out.println("\nThank you for using Book My Stay!");
+    }
 }
